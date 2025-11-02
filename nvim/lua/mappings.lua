49ed7a731982ed076eeb278fe3198719ee85f9c2
omeletteak <>
2025-11-02 01:48:08 +0000

    Feat: Add Python development keymaps
    Add key mappings for running the current Python file, running pytest, and opening a Python REPL.
    
    FossilOrigin-Name: a84bbdcd0a5e645dd14fec0dbcae0b0253fa975585fa91c1252b3b9d17e406cd

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,12 +1,37 @@
 require "nvchad.mappings"
 
 -- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
 map('t', '<Esc>', [[<C-\><C-n>]], { noremap = true, silent = true })
+
+-- Python 開発用マッピング
+map("n", "<leader>rr", function()
+  require("nvchad.term").runner {
+    pos = "sp",
+    cmd = "python " .. vim.fn.expand('%:p'),
+    clear_cmd = true,
+    id = "pythonRunner",
+  }
+end, { desc = "Run Python file" })
+
+map("n", "<leader>rt", function()
+  require("nvchad.term").runner {
+    pos = "sp",
+    cmd = "pytest " .. vim.fn.expand('%:p'),
+    clear_cmd = true,
+    id = "pytestRunner",
+  }
+end, { desc = "Run pytest" })
+
+map("n", "<leader>ri", function()
+  require("nvchad.term").toggle {
+    pos = "float",
+    cmd = "python",
+    id = "pythonREPL",
+  }
+end, { desc = "Open Python REPL" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
